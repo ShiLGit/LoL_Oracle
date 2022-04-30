747e51f70f2d285e7c11ee4919ebe9b069d756e2
lilianshi00@gmail.com <lilianshi00@gmail.com>
2022-04-30 19:57:15 -0400

    add basic theme, kind of fix ghetto navbar; todo = make landing page, make nav fully responsive

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { green, purple } from "@mui/material/colors";
+
 function App() {
+	const theme = createTheme({
+		palette: {
+			primary: {
+				main: "#241C55"
+			},
+			secondary: {
+				main: "#060117"
+			}
+		}
+	});
+
 	return (
-		<div className='Layout'>
-			<Navbar options={["Main", "About"]} />
-		</div>
+		<ThemeProvider theme={theme}>
+			<div className='Layout'>
+				<Navbar options={["Main", "About"]} />
+			</div>
+		</ThemeProvider>
 	);
 }
 
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,21 +1,71 @@
-import React from "react";
-import "./Navbar.css";
-
-import Typography from "@mui/material/Typography";
+import * as React from "react";
 import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+
 const Navbar = ({ options }) => {
+	const [anchorElNav, setAnchorElNav] = React.useState(null);
+	const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+	const handleOpenNavMenu = (event) => {
+		setAnchorElNav(event.currentTarget);
+	};
+	const handleOpenUserMenu = (event) => {
+		setAnchorElUser(event.currentTarget);
+	};
+
+	const handleCloseNavMenu = () => {
+		setAnchorElNav(null);
+	};
+
+	const handleCloseUserMenu = () => {
+		setAnchorElUser(null);
+	};
+
 	return (
-		<AppBar>
-			<Toolbar variant='string' disableGutters>
-				{options.map((opt) => (
-					<Typography variant='h5' component='div' gutterBottom sx={{ display: "flex", padding: "10px" }}>
-						{opt}
+		<AppBar position='static' sx={{ height: "fit-content" }}>
+			<Container maxWidth='xl'>
+				<Toolbar disableGutters>
+					<Typography variant='h6' noWrap component='div' sx={{ mr: 2, display: { xs: "none", md: "flex" } }}>
+						LoL Oracle
 					</Typography>
-				))}
-			</Toolbar>
+
+					<Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+						<IconButton
+							size='large'
+							aria-label='account of current user'
+							aria-controls='menu-appbar'
+							aria-haspopup='true'
+							onClick={handleOpenNavMenu}
+							color='inherit'
+						></IconButton>
+					</Box>
+					<Typography
+						variant='h6'
+						noWrap
+						component='div'
+						sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
+					>
+						LOGO
+					</Typography>
+					<Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+						{options.map((page) => (
+							<Button
+								key={page}
+								onClick={handleCloseNavMenu}
+								sx={{ my: 2, color: "white", display: "block" }}
+							>
+								{page}
+							</Button>
+						))}
+					</Box>
+				</Toolbar>
+			</Container>
 		</AppBar>
 	);
 };
-
 export default Navbar;
